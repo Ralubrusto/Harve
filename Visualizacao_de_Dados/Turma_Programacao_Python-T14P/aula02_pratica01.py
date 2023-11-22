a453717e50c891e8ab403033ccb857d5b6407dbe
Ralubrusto <ralubrusto.27@gmail.com>
2023-11-22 19:14:11 -0300

    Exercício prático

---
+++ Visualizacao_de_Dados/Turma_Programacao_Python-T14P/aula02_pratica01.py
@@ -0,0 +1,25 @@
+
+# Utilizem o dataset de populações para este exercício:
+
+# 01 - Plote um gráfico de linha com a população do Brasil em função do tempo
+#      - Extras:
+#           - Coloque título e nome nos eixos X e Y
+
+# 02 - Plote um único gráfico com a evolução da população de três países a sua escolha
+#      - Extras:
+#           - Coloque título e nome nos eixos X e Y
+#           - Coloque uma LEGENDA no gráfico que permita diferenciar a curva de cada país
+
+
+# 03 - Plote 2 gráficos de linha:
+#      - No primeiro plote a população da Argentina (em vermelho), do Uruguai (azul) e da Venezuela (em amarelo)
+#      - No segundo plote a população do Chile (tracejado), do Paraguay (pontilhado) e da Colombia (linha contínua)
+#      - Extras:
+#           - Coloque título e nome nos eixos X e Y de cada gráfico
+#           - Coloque uma LEGENDA em cada gráfico
+
+# 04 - DESAFIO:
+#      - Escolha 5 países da europa e plote a evolução de suas populações ao longo do tempo com as seguintes condições:
+#           - Plote apenas as populações de anos redondos (1960, 1970, 1980 ....)
+#           - Para cada país plote uma linha que também possua marcadores para destacar cada ponto ('*', '^', ...)
+#           - Coloque legenda e nome nos eixos
